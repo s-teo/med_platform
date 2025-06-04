7e80c5571bd980ac25a38e6d5f8f167bff4e7ee6
s-teo <tsultanov151@gmail.com>
2025-06-04 23:59:07 +0600

    alter appointments status

--- med_platform/apps/appointments/models.py
+++ med_platform/apps/appointments/models.py
@@ -3,11 +3,16 @@ from django.conf import settings
 from apps.doctors.models import Doctor, TimeSlot  # Импорт из нового приложения
 
 class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('scheduled', 'Запланировано'),
+        ('cancelled', 'Отменена'),
+        ('completed', 'Завершена'),
+    ]
     patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='appointments')
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='appointments')
     time_slot = models.OneToOneField(TimeSlot, on_delete=models.CASCADE)
     reason = models.TextField(blank=True)
-    status = models.CharField(max_length=20, choices=[('scheduled', 'Scheduled'), ('cancelled', 'Cancelled')], default='scheduled')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='scheduled')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
